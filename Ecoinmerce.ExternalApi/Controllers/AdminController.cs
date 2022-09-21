d549a2e29530a098c89e2ab8e20e03bf0eecdd43
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-09-21 16:21:03 -0300

    simple refactor

--- Ecoinmerce.ExternalApi/Controllers/AdminController.cs
+++ Ecoinmerce.ExternalApi/Controllers/AdminController.cs
@@ -1,15 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Ecoinmerce.ExternalApi.Controllers
+namespace Ecoinmerce.ExternalApi.Controllers;
+
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/admin/")]
+[ApiController]
+public class AdminController : ControllerBase
 {
-    [ApiVersion("1")]
-    [Route("api/v{version:apiVersion}/admin/")]
-    [ApiController]
-    public class AdminController : ControllerBase
-    {
-        // Delete data
-        // Add data
-        // Edit data
-    }
+    // Delete data
+    // Add data
+    // Edit data
 }
 
--- Ecoinmerce.ExternalApi/Controllers/ShoppingController.cs
+++ Ecoinmerce.ExternalApi/Controllers/ShoppingController.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Ecoinmerce.ExternalApi.Controllers
+namespace Ecoinmerce.ExternalApi.Controllers;
+
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/shopping/")]
+[ApiController]
+public class ShoppingController : ControllerBase
 {
-    [ApiVersion("1")]
-    [Route("api/v{version:apiVersion}/shopping/")]
-    [ApiController]
-    public class ShoppingController : ControllerBase
-    {
-        // Verify shopping payment (sender: ecommerce | need auth)
-        
-        // Receive notification of payment intent (sender: ecommerce | need auth | data: purchase ID, amount in R$, purchase date)
-    }
+    // Verify shopping payment (sender: ecommerce | need auth)
+    
+    // Receive notification of payment intent (sender: ecommerce | need auth | data: purchase ID, amount in R$, purchase date)
 }
