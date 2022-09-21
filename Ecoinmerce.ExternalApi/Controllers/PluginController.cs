78d60f41a6be65a5203b1cb82a80f137ee22de4d
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-09-21 16:21:44 -0300

    plugin controller created

---
+++ Ecoinmerce.ExternalApi/Controllers/PluginController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text;
+
+namespace Ecoinmerce.ExternalApi.Controllers;
+
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/plugin/")]
+[ApiController]
+public class PluginController : ControllerBase
+{
+    private readonly string _absoluteBasePath;
+    public PluginController()
+    {
+        var relativePath = ".\\PartnerPlugin\\Scripts\\";
+        _absoluteBasePath = Path.GetFullPath(relativePath);
+    }
+
+    private string ReadPluginFileFromFileName(string fileName)
+    {
+        return System.IO.File.ReadAllText(Path.Combine(_absoluteBasePath, fileName));
+    }
+
+    private class JavaScripResult : ContentResult
+    {
+        public JavaScripResult(string script)
+        {
+            StatusCode = 200;
+            Content = script;
+            ContentType = "text/javascript";
+        }
+    }
+
+    [HttpGet]
+    [Route("easy-plugin")]
+    public ContentResult GetEasyPlugin()
+    {
+        string javascript = ReadPluginFileFromFileName("easy-plugin.js");
+
+        return new JavaScripResult(javascript);
+    }
+}
