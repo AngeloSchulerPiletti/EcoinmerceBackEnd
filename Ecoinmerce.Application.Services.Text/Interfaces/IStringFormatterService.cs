b7a78391f54f7c59526051b59e88328107bfac78
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-10-07 15:53:32 -0300

    Service form string added

---
+++ Ecoinmerce.Application.Services.Text/Interfaces/IStringFormatterService.cs
@@ -0,0 +1,23 @@
+﻿namespace Ecoinmerce.Application.Services.Text.Interfaces
+{
+    public interface IStringFormatterService
+    {
+        /// <summary>
+        /// Formats a string to only numbers, non-accented letters and hyphen <br/>
+        /// Example: <br/>
+        /// INPUT: Maçã do amor. A máxima da branca de neve. COD.1265 <br/>
+        /// OUTPUT: maca-do-amor-a-maxima-da-branca-de-neve-cod1265
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public string FormatLowerNeutralString(string str);
+
+        /// <summary>
+        /// Removes the words accents. <br/>
+        /// Example: ç => c | ã => a | Ô => O 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public string RemoveAccent(string str);
+    }
+}
---
+++ Ecoinmerce.Application.Services.Text/StringFormatterService.cs
@@ -0,0 +1,43 @@
+﻿using Ecoinmerce.Application.Services.Text.Interfaces;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Ecoinmerce.Application.Services.Text
+{
+    public class StringFormatterService : IStringFormatterService
+    {
+        public string FormatLowerNeutralString(string str)
+        {
+            string initialHandle = str.ToLower()
+                                        .Replace(" ", "-")
+                                        .Replace(":", "-")
+                                        .Replace("/", "-")
+                                        .Replace("\\", "-")
+                                        .Replace("|", "-");
+
+            string accentRemoved = RemoveAccent(initialHandle);
+
+            string specialSimbolsRemoved = Regex.Replace(accentRemoved, @"[^a-zA-Z0-9\-]", "");
+
+            return specialSimbolsRemoved;
+        }
+
+        public string RemoveAccent(string str)
+        {
+            var normalizedString = str.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
