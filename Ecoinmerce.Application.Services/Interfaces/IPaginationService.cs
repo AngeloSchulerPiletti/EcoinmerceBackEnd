23a2734e2a8846fe4aa978cb5927299f6bc425f2
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-10-07 15:52:56 -0300

    Pagination Service created

---
+++ Ecoinmerce.Application.Services/Interfaces/IPaginationService.cs
@@ -0,0 +1,9 @@
+﻿using Ecoinmerce.Domain.Objects.DTOs.Requests;
+using Ecoinmerce.Domain.Objects.VOs.Responses;
+
+namespace Ecoinmerce.Application.Services.Interfaces;
+
+public interface IPaginationService
+{
+    public MessageBagSingleEntityVO<PaginationDTO> MapPagination(int page, int limit);
+}
---
+++ Ecoinmerce.Application.Services/PaginationService.cs
@@ -0,0 +1,15 @@
+﻿using Ecoinmerce.Application.Services.Interfaces;
+using Ecoinmerce.Domain.Objects.DTOs.Requests;
+using Ecoinmerce.Domain.Objects.VOs.Responses;
+
+namespace Ecoinmerce.Application.Services;
+
+public class PaginationService : IPaginationService
+{
+    public MessageBagSingleEntityVO<PaginationDTO> MapPagination(int page, int limit)
+    {
+        return  (page == 0 || limit == 0) ?
+            new MessageBagSingleEntityVO<PaginationDTO>("A paginação não pode ter página 0 ou limite 0", "Erro ao mapear a paginação", true, null, "P001") :
+            new MessageBagSingleEntityVO<PaginationDTO>("Mapeado com sucesso", null, false, new PaginationDTO(page, limit));
+    }
+}
