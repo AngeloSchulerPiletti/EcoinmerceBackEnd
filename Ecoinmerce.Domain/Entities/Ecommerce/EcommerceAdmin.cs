e172d0fddd7ec0d01cde6903768e6488b3999f98
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-10-07 15:58:35 -0300

    own methods handlers added

--- Ecoinmerce.Domain/Entities/Ecommerce/EcommerceAdmin.cs
+++ Ecoinmerce.Domain/Entities/Ecommerce/EcommerceAdmin.cs
@@ -1,30 +1,46 @@
 ﻿using Ecoinmerce.Domain.Entities.Interfaces;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
-namespace Ecoinmerce.Domain.Entities
+namespace Ecoinmerce.Domain.Entities;
+
+public class EcommerceAdmin : IBaseAuthenticable, IBaseConfirmable
 {
-    public class EcommerceAdmin : IBaseAuthenticable, IBaseConfirmable
+    public uint Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public bool IsEmailConfirmed { get; set; }
+    [JsonIgnore]
+    public byte[] Salt { get; set; }
+    [JsonIgnore]
+    public string Password { get; set; }
+    public string TokenConfirmation { get; set; }
+    public DateTime? TokenConfirmationExpiry { get; set; }
+    public string AccessToken { get; set; }
+    public DateTime? AccessTokenExpiry { get; set; }
+    public string RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiry { get; set; }
+    public bool IsDeleted { get; set; }
+    public uint EcommerceId { get; set; }
+    [JsonIgnore]
+    public virtual Ecommerce Ecommerce { get; set; }
+    [JsonIgnore]
+    public virtual List<RoleBond> RoleBonds { get; set; }
+
+    [NotMapped]
+    private string _stringfiedRoles { get; set; } = null;
+
+    public void CleanAccessToken()
     {
-        public uint Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public bool IsEmailConfirmed { get; set; }
-        [JsonIgnore]
-        public byte[] Salt { get; set; }
-        public string Password { get; set; }
-        public string TokenConfirmation { get; set; }
-        public DateTime? TokenConfirmationExpiry { get; set; }
-        public string AccessToken { get; set; }
-        public DateTime? AccessTokenExpiry { get; set; }
-        public string RefreshToken { get; set; }
-        public DateTime? RefreshTokenExpiry { get; set; }
-        public bool IsDeleted { get; set; }
-        public uint EcommerceId { get; set; }
-        [JsonIgnore]
-        public virtual Ecommerce Ecommerce { get; set; }
-        [JsonIgnore]
-        public virtual List<RoleBond> RoleBonds { get; set; }
+        AccessToken = null;
+        AccessTokenExpiry = null;
+    }
+
+    public string GetStringfiedRoles()
+    {
+        _stringfiedRoles ??= String.Join('+', RoleBonds);
+        return _stringfiedRoles;
     }
 }
--- Ecoinmerce.Domain/Entities/Ecommerce/EcommerceManager.cs
+++ Ecoinmerce.Domain/Entities/Ecommerce/EcommerceManager.cs
@@ -1,31 +1,37 @@
 ﻿using Ecoinmerce.Domain.Entities.Interfaces;
 using System.Text.Json.Serialization;
 
-namespace Ecoinmerce.Domain.Entities
+namespace Ecoinmerce.Domain.Entities;
+
+public class EcommerceManager : IBaseAuthenticable, IBaseConfirmable
 {
-    public class EcommerceManager : IBaseAuthenticable, IBaseConfirmable
+    public uint Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string Cpf { get; set; }
+    public string Cellphone { get; set; }
+    public string Phone { get; set; }
+    public bool IsDeleted { get; set; }
+    public string Email { get; set; }
+    public bool IsEmailConfirmed { get; set; }
+    public string TokenConfirmation { get; set; }
+    public DateTime? TokenConfirmationExpiry { get; set; }
+    public string AccessToken { get; set; }
+    public DateTime? AccessTokenExpiry { get; set; }
+    public string Password { get; set; }
+    [JsonIgnore]
+    public byte[] Salt { get; set; }
+    public string RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiry { get; set; }
+    public uint EcommerceId { get; set; }
+    [JsonIgnore]
+    public virtual Ecommerce Ecommerce { get; set; }
+
+    public void CleanAccessToken()
     {
-        public uint Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Username { get; set; }
-        public string Cpf { get; set; }
-        public string Cellphone { get; set; }
-        public string Phone { get; set; }
-        public bool IsDeleted { get; set; }
-        public string Email { get; set; }
-        public bool IsEmailConfirmed { get; set; }
-        public string TokenConfirmation { get; set; }
-        public DateTime? TokenConfirmationExpiry { get; set; }
-        public string AccessToken { get; set; }
-        public DateTime? AccessTokenExpiry { get; set; }
-        public string Password { get; set; }
-        [JsonIgnore]
-        public byte[] Salt { get; set; }
-        public string RefreshToken { get; set; }
-        public DateTime? RefreshTokenExpiry { get; set; }
-        public uint EcommerceId { get; set; }
-        [JsonIgnore]
-        public virtual Ecommerce Ecommerce { get; set; }
+        AccessToken = null;
+        AccessTokenExpiry = null;
     }
+
 }
