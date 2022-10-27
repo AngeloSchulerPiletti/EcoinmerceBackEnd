1e454debfa6780406bc8c965912bff5420c80af4
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-10-27 17:17:22 -0300

    added validate and maptoentity methods

--- Ecoinmerce.Application/Interfaces/IEcommerceBusiness.cs
+++ Ecoinmerce.Application/Interfaces/IEcommerceBusiness.cs
@@ -1,5 +1,11 @@
-﻿namespace Ecoinmerce.Application.Interfaces;
+﻿using Ecoinmerce.Domain.Entities;
+using Ecoinmerce.Domain.Objects.DTOs;
+using Ecoinmerce.Domain.Objects.VOs.Responses;
+
+namespace Ecoinmerce.Application.Interfaces;
 
 public interface IEcommerceBusiness
 {
+    public MessageBagSingleEntityVO<Ecommerce> MapToEntity(RegisterEcommerceDTO registerEcommerceDTO);
+    public MessageBagVO Validate(RegisterEcommerceDTO registerEcommerceDTO);
 }
--- Ecoinmerce.Application/Interfaces/IEcommerceManagerBusiness.cs
+++ Ecoinmerce.Application/Interfaces/IEcommerceManagerBusiness.cs
@@ -7,4 +7,6 @@ namespace Ecoinmerce.Application.Interfaces;
 public interface IEcommerceManagerBusiness
 {
     public MessageBagSingleEntityVO<EcommerceManager> Login(LoginDTO loginDTO);
+    public MessageBagSingleEntityVO<EcommerceManager> MapToEntity(RegisterManagerDTO registerManagerDTO);
+    public MessageBagVO Validate(RegisterManagerDTO registerManagerDTO);
 }
