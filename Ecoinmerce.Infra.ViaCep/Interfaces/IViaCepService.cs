c187908fb75411308a882eaaadcbf24e84d7214b
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-10-27 18:15:40 -0300

    ViaCep Api serivce adedd

---
+++ Ecoinmerce.Infra.ViaCep/Interfaces/IViaCepService.cs
@@ -0,0 +1,9 @@
+﻿
+using Ecoinmerce.Domain.Objects.VOs.Responses;
+
+namespace Ecoinmerce.Infra.ViaCep.Interfaces;
+
+public interface IViaCepService
+{
+    public ViaCepResponseVO GetCompleteAddress(string cep);
+}
---
+++ Ecoinmerce.Infra.ViaCep/ViaCepService.cs
@@ -0,0 +1,32 @@
+﻿using Ecoinmerce.Domain.Objects.VOs.Responses;
+using Ecoinmerce.Infra.ViaCep.Interfaces;
+using System.Text.Json;
+
+namespace Ecoinmerce.Infra.ViaCep;
+
+public class ViaCepService : IViaCepService
+{
+    private readonly string _baseUrl = "https://viacep.com.br";
+
+    public ViaCepResponseVO GetCompleteAddress(string cep)
+    {
+        string completeUrl = $"{_baseUrl}/ws/{cep}/json/";
+
+        HttpClient client = new();
+        HttpRequestMessage request = new()
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri(completeUrl)
+        };
+
+        HttpResponseMessage response = client.Send(request);
+        if (!response.IsSuccessStatusCode) return null;
+
+        string jsonResponse = response.Content.ReadAsStringAsync().Result;
+        ViaCepResponseVO viaCepResponse = JsonSerializer.Deserialize<ViaCepResponseVO>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+        return viaCepResponse.Erro == "true" ?
+            null :
+            viaCepResponse;
+    }
+}
