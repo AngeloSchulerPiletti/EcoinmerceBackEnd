5a4ba2041ff6676946409a93f02fe661a68bb91e
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-10-27 18:24:28 -0300

    ViaCepResponse Value Object created

---
+++ Ecoinmerce.Domain/Objects/VOs/Responses/ViaCepResponseVO.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecoinmerce.Domain.Objects.VOs.Responses;
+
+public class ViaCepResponseVO
+{
+    public string Cep { get; set; }
+    public string Logradouro { get; set; }
+    public string Complemento { get; set; }
+    public string Bairro { get; set; }
+    public string Localidade { get; set; }
+    public string Ibge { get; set; }
+    public string Uf { get; set; }
+    public string Ddd { get; set; }
+    public string Erro { get; set; }
+}
