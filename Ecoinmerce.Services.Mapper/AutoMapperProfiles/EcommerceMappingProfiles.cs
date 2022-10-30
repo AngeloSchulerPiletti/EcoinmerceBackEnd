62350782b29de1e7d91dcdfd49fbe5d9bcad0067
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-10-30 02:24:01 -0600

    Mappers created as profiles of AutoMapper

---
+++ Ecoinmerce.Services.Mapper/AutoMapperProfiles/EcommerceMappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Ecoinmerce.Domain.Entities;
+using Ecoinmerce.Domain.Objects.DTOs;
+
+namespace Ecoinmerce.Services.Mapper.AutoMapperProfiles;
+
+public class EcommerceMappingProfiles : Profile
+{
+    public EcommerceMappingProfiles()
+    {
+        CreateMap<RegisterEcommerceDTO, Ecommerce>();
+        CreateMap<RegisterManagerDTO, EcommerceManager>();
+    }
+}
