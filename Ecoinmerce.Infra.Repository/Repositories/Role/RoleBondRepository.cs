7e81fd0976eedbe9a05b8246f86e9f4d449ac9c7
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-10-31 14:10:01 -0900

    Repository added

---
+++ Ecoinmerce.Infra.Repository/Repositories/Role/RoleBondRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ecoinmerce.Domain.Entities;
+using Ecoinmerce.Infra.Repository.Database.Context;
+using Ecoinmerce.Infra.Repository.Interfaces;
+
+namespace Ecoinmerce.Infra.Repository.Repositories;
+
+public class RoleBondRepository : GenericRepository<RoleBond>, IRoleBondRepository
+{
+    private readonly EcommerceContext _ecommerceContext;
+
+    public RoleBondRepository(EcommerceContext ecommerceContext) : base(ecommerceContext)
+    {
+        _ecommerceContext = ecommerceContext;
+    }
+}
