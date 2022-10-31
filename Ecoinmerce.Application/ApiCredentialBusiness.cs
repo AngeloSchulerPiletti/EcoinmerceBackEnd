00ed9cc1742f7d6fb3352f1d18bfd4b0e97bc393
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-10-31 14:10:28 -0900

    api credential business created

---
+++ Ecoinmerce.Application/ApiCredentialBusiness.cs
@@ -0,0 +1,9 @@
+﻿using Ecoinmerce.Application.Interfaces;
+using Ecoinmerce.Infra.Repository.Interfaces;
+
+namespace Ecoinmerce.Application;
+
+public class ApiCredentialBusiness : IApiCredentialBusiness
+{
+    private readonly IApiCredentialRepository _apiCredentialRepository;
+}
