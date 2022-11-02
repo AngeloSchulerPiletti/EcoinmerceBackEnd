0cfe34b7f82ab39de5214919baf2364a7d6ff6e6
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-11-02 09:51:33 -0900

    api credential created with endpoints

--- Ecoinmerce.InternalApi/Controllers/ApiCredentialController.cs
+++ Ecoinmerce.InternalApi/Controllers/ApiCredentialController.cs
@@ -1,4 +1,8 @@
-﻿using Ecoinmerce.Application.Interfaces;
+﻿using Castle.Core.Resource;
+using Ecoinmerce.Application.Interfaces;
+using Ecoinmerce.Domain.Entities;
+using Ecoinmerce.Domain.Objects.VOs.Responses;
+using Ecoinmerce.InternalApi.ControllerAttributes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecoinmerce.InternalApi.Controllers;
@@ -14,4 +18,31 @@ public class ApiCredentialController : ControllerBase
     {
         _apiCredentialBusiness = apiCredentialBusiness;
     }
+
+    [AdminOrManagerAuth]
+    [HttpGet]
+    [Route("max-credentials")]
+    public IActionResult GetMaxCredentials()
+    {
+        return Ok(_apiCredentialBusiness.GetMaxCredentials());
+    }
+
+    [ManagerAuth]
+    [HttpPost]
+    [Route("create-credential")]
+    public IActionResult CreateCredential([FromBody] ApiCredential apiCredential)
+    {
+        EcommerceManager manager = (EcommerceManager)HttpContext.Items["Manager"];
+        Ecommerce ecommerce = manager.Ecommerce;
+
+        MessageBagVO messageBagMaxCredentialsValidate = _apiCredentialBusiness.ValidateMaxCredentials(ecommerce);
+        if (messageBagMaxCredentialsValidate.IsError) return BadRequest(messageBagMaxCredentialsValidate);
+
+        MessageBagVO messageBagValidation = _apiCredentialBusiness.ValidateNewApiCredential(apiCredential);
+        if(messageBagValidation.IsError) return BadRequest(messageBagValidation);
+
+        MessageBagSingleEntityVO<ApiCredential> messageBagApiCredential = _apiCredentialBusiness.CreateApiCredential(ecommerce, apiCredential);
+        return messageBagApiCredential.IsError ? BadRequest(messageBagApiCredential) : Ok(messageBagApiCredential);
+    }
+
 }
