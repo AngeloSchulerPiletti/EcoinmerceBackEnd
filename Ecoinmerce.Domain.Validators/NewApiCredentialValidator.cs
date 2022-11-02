f4059427df9af274a4b597f8e00cf6d4d0951687
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-11-02 09:49:25 -0900

    apiCredential Validator

---
+++ Ecoinmerce.Domain.Validators/NewApiCredentialValidator.cs
@@ -0,0 +1,42 @@
+﻿using Ecoinmerce.Domain.Entities;
+using FluentValidation;
+
+namespace Ecoinmerce.Domain.Validators;
+
+public class NewApiCredentialValidator : AbstractValidator<ApiCredential>
+{
+    public NewApiCredentialValidator()
+    {
+        RuleFor(x => x.AccessToken)
+            .Empty().WithMessage("Não preencha esse campo");
+
+        RuleFor(x => x.AccessTokenExpiry)
+            .Empty().WithMessage("Não preencha esse campo");
+
+        RuleFor(x => x.UpdatedAt)
+            .Empty().WithMessage("Não preencha esse campo");
+
+        RuleFor(x => x.UpdatedBy)
+            .Empty().WithMessage("Não preencha esse campo");
+
+        RuleFor(x => x.CreatedAt)
+            .Empty().WithMessage("Não preencha esse campo");
+
+        RuleFor(x => x.CreatedBy)
+            .Empty().WithMessage("Não preencha esse campo");
+
+        RuleFor(x => x.EcommerceId)
+            .Empty().WithMessage("Não preencha esse campo");
+
+        RuleFor(x => x.Id)
+            .Empty().WithMessage("Não preencha esse campo");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Preencha esse campo")
+            .MaximumLength(50).WithMessage("Esse campo deve ter até caracteres");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(200).WithMessage("Esse campo deve ter até caracteres");
+
+    }
+}
