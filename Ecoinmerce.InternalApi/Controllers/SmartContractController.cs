e24622ebfc358db93131e5176f5d074cd3a1179d
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-11-02 05:43:45 -0900

    GetAddress endpoints created on smart contract controllers

---
+++ Ecoinmerce.InternalApi/Controllers/SmartContractController.cs
@@ -0,0 +1,24 @@
+﻿using Ecoinmerce.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecoinmerce.InternalApi.Controllers;
+
+
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/smart-contract/")]
+[ApiController]
+public class SmartContractController : ControllerBase
+{
+    private readonly ISmartContractBusiness _smartContractBusiness;
+    public SmartContractController(ISmartContractBusiness smartContractBusiness)
+    {
+        _smartContractBusiness = smartContractBusiness;
+    }
+
+    [HttpGet]
+    [Route("address")]
+    public IActionResult GetAddress()
+    {
+        return Ok(_smartContractBusiness.GetSmartContractAddress());
+    }
+}
