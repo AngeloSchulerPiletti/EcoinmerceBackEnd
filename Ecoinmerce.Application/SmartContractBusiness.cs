7ca29f1a86d24d633e2975e04a1cbb4b788c151a
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-11-02 09:59:38 -0900

    setting file implemented as IOptions<>

--- Ecoinmerce.Application/SmartContractBusiness.cs
+++ Ecoinmerce.Application/SmartContractBusiness.cs
@@ -1,18 +1,19 @@
 ﻿using Ecoinmerce.Application.Interfaces;
 using Ecoinmerce.Infra.Blockchain;
+using Microsoft.Extensions.Options;
 
 namespace Ecoinmerce.Application;
 
 public class SmartContractBusiness : ISmartContractBusiness
 {
-    private readonly BlockchainSettings _blockchainSettings;
-    public SmartContractBusiness(BlockchainSettings blockchainSettings)
+    private readonly IOptions<BlockchainSettings> _blockchainSettings;
+    public SmartContractBusiness(IOptions<BlockchainSettings> blockchainSettings)
     {
         _blockchainSettings = blockchainSettings;
     }
 
     public string GetSmartContractAddress()
     {
-        return _blockchainSettings.SmartContract.Address;
+        return _blockchainSettings.Value.SmartContract.Address;
     }
 }
