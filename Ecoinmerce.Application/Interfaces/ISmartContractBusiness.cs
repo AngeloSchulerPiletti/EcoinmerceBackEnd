ee1f5be9939f61074993e71f6721f7ffc4b004e5
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-11-02 05:39:50 -0900

    SmartContractBusiness created with getter for smart contract address

---
+++ Ecoinmerce.Application/Interfaces/ISmartContractBusiness.cs
@@ -0,0 +1,8 @@
+﻿using Ecoinmerce.Domain.Objects.VOs.Responses;
+
+namespace Ecoinmerce.Application.Interfaces;
+
+public interface ISmartContractBusiness
+{
+    public string GetSmartContractAddress();
+}
---
+++ Ecoinmerce.Application/SmartContractBusiness.cs
@@ -0,0 +1,19 @@
+﻿using Ecoinmerce.Application.Interfaces;
+using Ecoinmerce.Domain.Objects.VOs.Responses;
+using Ecoinmerce.Infra.Blockchain;
+
+namespace Ecoinmerce.Application;
+
+public class SmartContractBusiness : ISmartContractBusiness
+{
+    private readonly BlockchainSettings _blockchainSettings;
+    public SmartContractBusiness(BlockchainSettings blockchainSettings)
+    {
+        _blockchainSettings = blockchainSettings;
+    }
+
+    public string GetSmartContractAddress()
+    {
+        return _blockchainSettings.SmartContract.Address;
+    }
+}
