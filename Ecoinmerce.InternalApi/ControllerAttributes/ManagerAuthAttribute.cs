2b402564c38de1dece86c71db90c8aef04a01468
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-11-02 08:54:22 -0900

    var rename

--- Ecoinmerce.InternalApi/ControllerAttributes/ManagerAuthAttribute.cs
+++ Ecoinmerce.InternalApi/ControllerAttributes/ManagerAuthAttribute.cs
@@ -10,11 +10,11 @@ public class ManagerAuthAttribute : Attribute, IAuthorizationFilter
 {
     public void OnAuthorization(AuthorizationFilterContext context)
     {
-        EcommerceManager admin = (EcommerceManager)context.HttpContext.Items["Manager"];
+        EcommerceManager manager = (EcommerceManager)context.HttpContext.Items["Manager"];
         bool isAccessTokenExpired = (bool)context.HttpContext.Items["IsAccessTokenExpired"];
         bool isAccessTokenValid = (bool)context.HttpContext.Items["IsAccessTokenValid"];
 
-        if (admin == null)
+        if (manager == null)
             context.Result = new JsonResult(new MessageBagVO("Faça o login para acessar", "Unauthorized", true, "L004")) { StatusCode = StatusCodes.Status401Unauthorized };
         else if (!isAccessTokenValid)
             context.Result = new JsonResult(new MessageBagVO("Token inválido", "Unauthorized", true, "L005")) { StatusCode = StatusCodes.Status401Unauthorized };
