92aaea3d2c48abd5520d847144814fd5a5b1cf5b
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-11-11 22:00:58 -0300

    Refactor

--- Ecoinmerce.Domain.Validators/NewApiCredentialValidator.cs
+++ Ecoinmerce.Domain.Validators/NewApiCredentialValidator.cs
@@ -33,14 +33,18 @@ public class NewApiCredentialValidator : AbstractValidator<ApiCredential>
 
         RuleFor(x => x.ValidityInDays)
             .NotEmpty().WithMessage("Preencha esse campo")
-            .Must(x => x > 0 && x < 90).WithMessage("A credencial deve durar de 1 à 90 dias");
+            .DependentRules(() =>
+            {
+                RuleFor(x => x.ValidityInDays)
+                    .Must(x => x > 0 && x < 90).WithMessage("A credencial deve durar de 1 à 90 dias");
+            });
 
         RuleFor(x => x.Name)
             .NotEmpty().WithMessage("Preencha esse campo")
-            .MaximumLength(50).WithMessage("Esse campo deve ter até 50 caracteres");
+            .MaximumLength(50).WithMessage("Até 50 caracteres");
 
         RuleFor(x => x.Description)
-            .MaximumLength(200).WithMessage("Esse campo deve ter até 200 caracteres");
+            .MaximumLength(200).WithMessage("Até 200 caracteres");
 
     }
 }
