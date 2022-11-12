b29f8a074515f3eca79db2614384afd19250dd1c
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-11-11 22:50:56 -0300

    Validator changed to support base identifiers and list of error for a given property

--- Ecoinmerce.Domain.Validators/GenericValidatorExecutor.cs
+++ Ecoinmerce.Domain.Validators/GenericValidatorExecutor.cs
@@ -7,16 +7,26 @@ namespace Ecoinmerce.Domain.Validators;
 
 public class GenericValidatorExecutor : IGenericValidatorExecutor
 {
-    public MessageBagVO ValidatorResultIterator<TEntity>(TEntity content, AbstractValidator<TEntity> validator) where TEntity : class
+    public MessageBagVO ValidatorResultIterator<TEntity>(TEntity content, AbstractValidator<TEntity> validator, string baseIdentifier = null) where TEntity : class
     {
         ValidationResult contentResult = validator.Validate(content);
         if (!contentResult.IsValid)
         {
             MessageBagSingleEntityVO<TEntity> errors = new();
+            Dictionary<string, object> errorsDictionary = new();
+
             foreach (ValidationFailure failure in contentResult.Errors)
             {
-                errors.DictionaryMessages.Add(failure.PropertyName, failure.ErrorMessage);
+                string propertyNameFormatted = char.ToLower(failure.PropertyName[0]) + failure.PropertyName.Substring(1);
+                if (!errorsDictionary.Any(x => x.Key == propertyNameFormatted))
+                    errorsDictionary.Add(propertyNameFormatted, new List<string>());
+
+                ((List<string>)errorsDictionary[propertyNameFormatted]).Add(failure.ErrorMessage);
             }
+
+            if (baseIdentifier == null) errors.DictionaryMessages = errorsDictionary;
+            else errors.DictionaryMessages.Add(baseIdentifier, errorsDictionary);
+
             return errors;
         }
         return new MessageBagSingleEntityVO<TEntity>("Conteúdo validado", null, false);
--- Ecoinmerce.Domain.Validators/Interfaces/IGenericValidatorExecutor.cs
+++ Ecoinmerce.Domain.Validators/Interfaces/IGenericValidatorExecutor.cs
@@ -5,5 +5,5 @@ namespace Ecoinmerce.Domain.Validators.Interfaces;
 
 public interface IGenericValidatorExecutor
 {
-    public MessageBagVO ValidatorResultIterator<TEntity>(TEntity content, AbstractValidator<TEntity> validator) where TEntity : class;
+    public MessageBagVO ValidatorResultIterator<TEntity>(TEntity content, AbstractValidator<TEntity> validator, string baseIdentifier = null) where TEntity : class;
 }
