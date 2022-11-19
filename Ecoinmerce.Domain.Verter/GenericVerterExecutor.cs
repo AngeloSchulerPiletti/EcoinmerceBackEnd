1939ecb5a3e0a77021d69064ecc12ab53bfef267
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-11-19 19:31:16 -0300

    Verter generic static class created

---
+++ Ecoinmerce.Domain.Verter/GenericVerterExecutor.cs
@@ -0,0 +1,30 @@
+﻿using Ecoinmerce.Domain.Objects.VOs.Responses;
+using System.Reflection;
+
+namespace Ecoinmerce.Domain.Verter;
+
+public static class GenericVerterExecutor
+{
+    public static MessageBagSingleEntityVO<TEntity> UpdateEntityFromObject<TEntity, TObject>(TObject objectEntity, TEntity entity, List<string> ignoredProperties = null) where TEntity : class where TObject : class
+    {
+        PropertyInfo[] propertyInfos = typeof(TObject).GetProperties();
+        foreach (PropertyInfo propertyInfo in propertyInfos)
+        {
+            string propertyName = propertyInfo.Name;
+
+            if (ignoredProperties != null && !ignoredProperties.Contains(propertyName))
+            {
+                PropertyInfo customerProperty = typeof(TEntity).GetProperty(propertyName);
+                if (customerProperty != null)
+                {
+                    object value = propertyInfo.GetValue(objectEntity);
+                    if (value != null)
+                    {
+                        customerProperty.SetValue(entity, value);
+                    }
+                }
+            }
+        }
+        return new MessageBagSingleEntityVO<TEntity>("Campos mapeados", "Sucesso", false, entity);
+    }
+}
