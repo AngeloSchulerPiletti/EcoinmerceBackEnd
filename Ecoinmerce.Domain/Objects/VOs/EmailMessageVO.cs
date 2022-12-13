5681496cdc4f20e5ece8f77fa7fe217cd30bc09a
angelo <angelopiletti@gmail.com>
2022-12-13 11:54:20 -0300

    Base contact controller and message entity done

---
+++ Ecoinmerce.Domain/Objects/VOs/EmailMessageVO.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecoinmerce.Domain.Objects.VOs;
+
+public class EmailMessageVO
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Subject { get; set; }
+    public string Cellphone { get; set; }
+    public string Text { get; set; }
+}
---
+++ Ecoinmerce.InternalApi/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using Ecoinmerce.Domain.Objects.VOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecoinmerce.InternalApi.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/contact/")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        [HttpPost]
+        [Route("from-home")]
+        public IActionResult FromHome([FromBody] EmailMessageVO emailMessage)
+        {
+            return Ok("Mensagem não enviada porque o Angelo ainda não implementou, mas o endpoint existe");
+        }
+    }
+}
