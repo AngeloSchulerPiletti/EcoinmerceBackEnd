3ebc35353feeacc6c2ae87c32cdb5f1a533975c0
angelo <angelopiletti@gmail.com>
2022-12-30 19:50:25 -0300

    param rename

--- Ecoinmerce.ExternalApi/Controllers/PopUpController.cs
+++ Ecoinmerce.ExternalApi/Controllers/PopUpController.cs
@@ -8,7 +8,7 @@ public class PopUpController : Controller
 {
     [HttpGet]
     [Route("ether/easy-popup")]
-    public IActionResult GetEtherEasyPopUp([FromQuery] string amountInEther, [FromQuery] string purchaseIdentifier, [FromQuery] uint ecommerceId)
+    public IActionResult GetEtherEasyPopUp([FromQuery] string purchaseTotal, [FromQuery] string purchaseIdentifier, [FromQuery] uint ecommerceId)
     {
 
         return View("~/PopUp/Ether/Index.cshtml");
