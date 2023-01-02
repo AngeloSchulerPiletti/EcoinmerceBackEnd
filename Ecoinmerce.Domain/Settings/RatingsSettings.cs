b87d527bcd4b7f1df40d026c354892c1f32855c4
angelo <angelopiletti@gmail.com>
2023-01-02 16:42:22 -0300

    RatingsSettings Class for settings done

---
+++ Ecoinmerce.Domain/Settings/RatingsSettings.cs
@@ -0,0 +1,32 @@
+﻿namespace Ecoinmerce.Domain.Settings;
+
+public class RatingsSettings
+{
+    public RatingsApiSettings CoinMarketCap { get; set; }
+    public RatingsApiSettings Coinbase { get; set; }
+    public RatingsApiSettings Etherscan { get; set; }
+}
+
+public class RatingsApiSettings
+{
+    public string AccessToken { get; set; }
+    public string BaseUrl { get; set; }
+}
+
+public class RatingCode
+{
+    public string CoinMarketCapCode;
+
+    public RatingCode(string coinMarketCapCode)
+    {
+        CoinMarketCapCode = coinMarketCapCode;
+    }
+}
+
+
+public static class RatingsCodes
+{
+    public static RatingCode Ether = new ("ETH");
+    public static RatingCode Bitcoin = new ("BTC");
+    public static RatingCode BrazilianReal = new ("BRL");
+}
