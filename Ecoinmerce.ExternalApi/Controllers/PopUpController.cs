abaa7215e21efc7f0aa797f089dcd61241e0433e
Lucas Oliveira <lucas.oliveira@zallpy.com>
2023-01-03 17:56:55 -0300

    Add constructor

--- Ecoinmerce.ExternalApi/Controllers/PopUpController.cs
+++ Ecoinmerce.ExternalApi/Controllers/PopUpController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ecoinmerce.Application.Interfaces;
+using Ecoinmerce.Domain.Objects.VOs.Responses;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ecoinmerce.ExternalApi.Controllers;
 
@@ -6,11 +8,18 @@ namespace Ecoinmerce.ExternalApi.Controllers;
 [Route("api/v{version:apiVersion}/popup/")]
 public class PopUpController : Controller
 {
+    private readonly IEcommerceBusiness _ecommerceBusiness;
+
+    public PopUpController(IEcommerceBusiness ecommerceBusiness)
+    {
+        _ecommerceBusiness = ecommerceBusiness;
+    }
+
     [HttpGet]
     [Route("ether/easy-popup")]
     public IActionResult GetEtherEasyPopUp([FromQuery] string purchaseTotal, [FromQuery] string purchaseIdentifier, [FromQuery] uint ecommerceId)
     {
-
+        ViewBag.purchaseTotal = purchaseTotal;
         return View("~/PopUp/Ether/Index.cshtml");
     }
 }
