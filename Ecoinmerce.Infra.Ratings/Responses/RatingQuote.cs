89283fcce37f7786d5ae3d0b13756be16275892b
angelo <angelopiletti@gmail.com>
2023-01-03 10:31:01 -0300

    RatingQuote created

---
+++ Ecoinmerce.Infra.Ratings/Responses/RatingQuote.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecoinmerce.Infra.Ratings.Responses;
+
+public class RatingQuote
+{
+    public decimal Price { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public string CurrencyFrom { get; set; }
+    public string CurrencyTo { get; set; }
+}
