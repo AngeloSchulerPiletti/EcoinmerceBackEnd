4ac2e452fcc2197c91b9d99371955b4770e088e4
angelo <angelopiletti@gmail.com>
2023-01-03 18:25:54 -0300

    Midia Controller created

---
+++ Ecoinmerce.ExternalApi/Controllers/MidiaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecoinmerce.ExternalApi.Controllers;
+
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/midia/")]
+[ApiController]
+public class MidiaController : ControllerBase
+{
+    [HttpGet]
+    [Route("brand")]
+    public IActionResult GetMidia()
+    {
+
+    }
+}
