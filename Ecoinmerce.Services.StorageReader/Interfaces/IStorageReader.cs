6d8a9023bc173390cf2c1530a3c0a70b8de4642e
angelo <angelopiletti@gmail.com>
2023-01-04 14:49:27 -0300

    Fixing path to disk

--- Ecoinmerce.Services.StorageReader/Interfaces/IStorageReader.cs
+++ Ecoinmerce.Services.StorageReader/Interfaces/IStorageReader.cs
@@ -1,5 +1,11 @@
-﻿namespace Ecoinmerce.Services.StorageReader.Interfaces;
+﻿using Ecoinmerce.Domain.Settings;
+using System.Text;
+
+namespace Ecoinmerce.Services.StorageReader.Interfaces;
 
 public interface IStorageReader
 {
+    public string GetImageMediaType(string fileFullPathName);
+    public string GetMidiaFileFullName(string fileName);
+    public byte[] GetMidiaFile(string fullFileName);
 }
--- Ecoinmerce.Services.StorageReader/StorageReader.cs
+++ Ecoinmerce.Services.StorageReader/StorageReader.cs
@@ -1,14 +1,49 @@
 ﻿using Ecoinmerce.Domain.Settings;
 using Ecoinmerce.Services.StorageReader.Interfaces;
+using System.Text;
 
 namespace Ecoinmerce.Services.StorageReader;
 
 public class StorageReader : IStorageReader
 {
     private readonly StorageSettings _storageSettings;
+    private readonly string _baseDir;
+    private readonly string _diskRoot;
 
     public StorageReader(StorageSettings storageSettings)
     {
         _storageSettings = storageSettings;
+
+        string runningDirectory = Environment.CurrentDirectory;
+        _diskRoot = Path.GetPathRoot(runningDirectory);
+        _baseDir = Path.Combine(_diskRoot, _storageSettings.RootFolder);
+    }
+
+
+    public string GetImageMediaType(string fileFullPathName)
+    {
+        FileInfo fileInfo = new(fileFullPathName);
+
+        StringBuilder stringBuilderMediaType = new();
+        stringBuilderMediaType.Append("image/");
+        stringBuilderMediaType.Append(fileInfo.Extension[1..]);
+        return stringBuilderMediaType.ToString();
+    }
+
+    public string GetMidiaFileFullName(string fileName)
+    {
+        return Path.Combine(_baseDir, _storageSettings.MidiaFolder, fileName);
+    }
+
+    public byte[] GetMidiaFile(string fullFileName)
+    {
+        try
+        {
+            return File.ReadAllBytes(fullFileName);
+        }
+        catch (Exception)
+        {
+            return null;
+        }
     }
 }
