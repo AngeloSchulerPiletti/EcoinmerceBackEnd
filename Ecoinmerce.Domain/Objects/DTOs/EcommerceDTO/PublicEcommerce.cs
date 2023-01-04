d644a74f6b5380cea167c3fb20af69095f519f7c
angelo <angelopiletti@gmail.com>
2023-01-03 21:20:07 -0300

    Object for public ecommerce info done

---
+++ Ecoinmerce.Domain/Objects/DTOs/EcommerceDTO/PublicEcommerce.cs
@@ -0,0 +1,7 @@
+﻿namespace Ecoinmerce.Domain.Objects.DTOs.EcommerceDTO;
+
+public record PublicEcommerce
+{
+    public string FantasyName { get; set; }
+    public string WalletAddress { get; set; }
+}
