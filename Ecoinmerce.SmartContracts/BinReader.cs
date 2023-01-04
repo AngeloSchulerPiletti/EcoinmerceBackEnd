d93d375c881cd4011399a4407e032c57d8d6e042
angelo <angelopiletti@gmail.com>
2023-01-03 21:47:56 -0300

    Fixed

--- Ecoinmerce.SmartContracts/BinReader.cs
+++ Ecoinmerce.SmartContracts/BinReader.cs
@@ -1,24 +1,44 @@
 ﻿using Ecoinmerce.SmartContracts.Interfaces;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Ecoinmerce.SmartContracts;
 
 public class BinReader : IBinReader
 {
     private readonly string _smartContractName;
+    private readonly string _baseDir;
     public BinReader()
     {
-        string[] fileNames = Directory.GetFiles("SmartContracts", @"\^[^I].+$\");
-        if (fileNames.Length == 0)
-            throw new Exception("SmartContract não encontrado");
-        _smartContractName = fileNames[0];
+        string runningDirectory = Environment.CurrentDirectory;
+        string solutionDirectory = Directory.GetParent(runningDirectory).FullName;
+        _baseDir = Path.Combine(solutionDirectory, "Ecoinmerce.SmartContracts");
+
+        string[] fullFileNames = Directory.GetFiles(Path.Combine(_baseDir, "SmartContracts"), "*.sol").ToArray();
+
+        _smartContractName = null;
+        for (int i = 0; i < fullFileNames.Length; i++)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(fullFileNames[i]);
+            if (fileName[0] != 'I')
+            {
+                _smartContractName = fileName;
+                break;
+            }
+
+        }
+
+        if (_smartContractName == null)
+            throw new Exception("SmartContract não encontrado, ou mais de um encontrado");
     }
 
     public string GetSmartContractJson()
     {
         StringBuilder stringBuilder = new();
 
-        stringBuilder.Append("SmartContracts/");
+        stringBuilder.Append(_baseDir);
+        stringBuilder.Append("/SmartContracts/bin/");
         stringBuilder.Append(_smartContractName);
         stringBuilder.Append(".json");
 
